4d57f0cabaca36742e6046d6741d1052b0993f38
Sakura520222 <sakura520222@outlook.com>
2026-03-09 19:28:10 +0800

    feat(core): 升级项目版本至 1.7.3

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ from core.settings import get_admin_list, get_bot_token, validate_required_setti
 from core.system.process_manager import start_qa_bot, stop_qa_bot
 
 # 版本信息
-__version__ = "1.7.2"
+__version__ = "1.7.3"
 
 
 async def graceful_shutdown_resources():
